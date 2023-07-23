7ba4962e77f19832265541579bbea4e5dcd19040
İkram Dağcı <ikramdgc@gmail.com>
2023-07-23 15:55:49 +0300

    feat(jwt) : Generate token with extra claims

--- src/main/java/com/ikramdagci/security/config/JwtService.java
+++ src/main/java/com/ikramdagci/security/config/JwtService.java
@@ -2,11 +2,16 @@ package com.ikramdagci.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -15,9 +20,22 @@ public class JwtService {
 
 
     public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
 
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
 
-        return null;
+    public String generateToken(Map<String,Object> extraClaims, UserDetails userDetails){
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token) {
