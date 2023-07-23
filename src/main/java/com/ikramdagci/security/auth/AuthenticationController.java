280f651866cb4792e50025521e683083cd8c4cec
İkram Dağcı <ikramdgc@gmail.com>
2023-07-23 17:21:42 +0300

    feat(jwt) Authentication Service

--- src/main/java/com/ikramdagci/security/auth/AuthenticationController.java
+++ src/main/java/com/ikramdagci/security/auth/AuthenticationController.java
@@ -12,14 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    private final AuthenticationService service;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest){
-// TODO: 23.07.2023
+        return ResponseEntity.ok(service.register(registerRequest));
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest registerRequest){
-
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest){
+        return ResponseEntity.ok(service.authenticate(authenticationRequest));
     }
 
 
--- src/main/java/com/ikramdagci/security/auth/AuthenticationService.java
+++ src/main/java/com/ikramdagci/security/auth/AuthenticationService.java
@@ -1,4 +1,42 @@
 package com.ikramdagci.security.auth;
 
+import com.ikramdagci.security.config.JwtService;
+import com.ikramdagci.security.user.Role;
+import com.ikramdagci.security.user.User;
+import com.ikramdagci.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest registerRequest) {
+        var user = User.builder()
+                .firstname(registerRequest.getFirstname())
+                .lastname(registerRequest.getLastname())
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return new AuthenticationResponse(jwtToken);
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(), authenticationRequest.getPassword()));
+        var user = userRepository.findByEmail(authenticationRequest.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return new AuthenticationResponse(jwtToken);
+    }
 }
