5881c055f76e0d0c4efa0a29f1fe551fa642e559
İkram Dağcı <ikramdgc@gmail.com>
2023-07-23 19:10:58 +0300

    feat(roles) : Added new roles and permissions as enum

--- src/main/java/com/ikramdagci/security/user/Role.java
+++ src/main/java/com/ikramdagci/security/user/Role.java
@@ -1,7 +1,19 @@
 package com.ikramdagci.security.user;
 
-public enum Role {
-    USER,
-    ADMIN
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+import java.util.Collections;
+import java.util.Set;
+
+import static com.ikramdagci.security.user.Permission.*;
+
+@RequiredArgsConstructor
+public enum Role {
+    USER(Collections.emptySet()),
+    ADMIN(Set.of(ADMIN_READ,ADMIN_UPDATE,ADMIN_DELETE,ADMIN_CREATE,MANAGER_READ,MANAGER_UPDATE,MANAGER_DELETE,MANAGER_CREATE)),
+    MANAGER(Set.of(MANAGER_READ,MANAGER_UPDATE,MANAGER_DELETE,MANAGER_CREATE));
+
+    @Getter
+    private final Set<Permission> permisions;
 }
